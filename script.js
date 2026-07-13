4436ee3943cef9000cd0ec6afaf1161310e327dd
Mayn18 <maynaik1865@gmail.com>
2026-07-13 20:44:11 +0530

    Added a Button to create a canvas of custom size

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
 const container = document.querySelector(".container");
-
+const gridBtn = document.querySelector("#customGrid");
 
 console.log(container);
 let arr = [];
 
-for(let i = 0; i < 16; i++){
-    arr[i] = [];
-    for(let j = 0; j < 16; j++){
-        const div = document.createElement("div");
-        div.classList.add("child");
-        arr[i][j] = div;
-    }
-}
+let gridIndex = 16;
 
-for(let i = 0; i < 16; i++){
-    const rowDiv = document.createElement("div");
-    for(let j = 0; j < 16; j++){
-        arr[i][j].style.height = "10px";
-        arr[i][j].style.width = "10px";
-        rowDiv.appendChild(arr[i][j]);
-        if(j === 15){
-            container.appendChild(rowDiv);
+function createGrid(girdIndex){
+    for(let i = 0; i < girdIndex; i++){
+        arr[i] = [];
+        for(let j = 0; j < gridIndex; j++){
+            const div = document.createElement("div");
+            div.classList.add("child");
+            arr[i][j] = div;
+        }
+    }
+    
+    for(let i = 0; i < gridIndex; i++){
+        const rowDiv = document.createElement("div");
+        for(let j = 0; j < gridIndex; j++){
+            arr[i][j].style.height = "10px";
+            arr[i][j].style.width = "10px";
+            rowDiv.appendChild(arr[i][j]);
+            if(j === gridIndex-1){
+                container.appendChild(rowDiv);
+            }
+        }
+    }
+    
+    for(let i = 0; i < gridIndex; i++){
+        for(let j = 0; j < gridIndex; j++){
+            arr[i][j].addEventListener("mouseover", () => { 
+                arr[i][j].style.backgroundColor = "yellow";
+            })
         }
     }
 }
 
-for(let i = 0; i < 16; i++){
-    for(let j = 0; j < 16; j++){
-        arr[i][j].addEventListener("mouseover", () => { 
-            arr[i][j].style.backgroundColor = "yellow";
-        })
+function takeUserInput(){
+    let temp = Number(prompt("Enter a number less than or equal to 100: ", 16));
+
+    if(!Number.isInteger(temp)){ 
+        alert("Enter a number!");
+        return takeUserInput();
     }
+    
+    if(temp > 100){
+        alert("Enter a number less than 100");
+        return takeUserInput();
+    }
+    return temp;
 }
+createGrid(gridIndex);
+
+gridBtn.addEventListener("click", ()=>{
+    gridIndex = takeUserInput();
+    console.log(gridIndex);
+    container.replaceChildren();
+    createGrid(gridIndex);
+});
